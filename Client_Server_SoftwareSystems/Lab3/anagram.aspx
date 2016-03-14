819a5c8cb302ceeb9f6a97ad13f4d3537dbfd07b
hitesh3004 <hnlaware@bu.edu>
2016-03-14 00:34:03 -0400

    Lab 3 for EC 512

---
+++ Client_Server_SoftwareSystems/Lab3/anagram.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="anagram.aspx.cs" Inherits="anagram" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Get Anagrams</title>
+    <style type="text/css">
+        .auto-style1 {
+            margin-left: 39px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div style="background-color: #C0C0C0; border-style: solid; border-width: 1px; margin: auto; width: 800px; padding: 10px">
+        <h1 style="text-align: center">Find Anagrams</h1>
+        Enter a character string:&nbsp;
+
+        <asp:TextBox ID="txt1" runat="server" style="width:147px;" ></asp:TextBox>
+        <asp:CheckBox ID="CheckBox1" runat="server" OnCheckedChanged="CheckBox1_CheckedChanged" />
+        <asp:Label ID="Label1" runat="server" Text="Label">Eliminate Duplicates</asp:Label>
+    
+        <br />
+        <br />
+        <br />
+    
+        <asp:ListBox ID="ListBox1" runat="server" Height="124px" OnSelectedIndexChanged="ListBox1_SelectedIndexChanged" style="height:85px;width:150px;"></asp:ListBox>
+        <br />
+        <br />
+        <asp:Button OnClick="calculate" Text="Find Anagrams" runat="server" CssClass="auto-style1" Height="23px" Width="112px" />
+        <p>
+            <asp:Label ID="Label2" runat="server" Text=""></asp:Label>
+        </p>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Client_Server_SoftwareSystems/Lab3/anagram.aspx.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+public partial class anagram : System.Web.UI.Page
+{
+    List<string> duplicate = new List<string>();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void calculate(object sender, EventArgs e)
+    {
+        String s = txt1.Text;
+        ListBox1.Items.Clear();
+        duplicate.Clear();
+        if (s.Length > 8){
+            Label2.Text = "Please enter a string from 1 to 8 characters";
+        }
+        else if (s.Length == 0){
+            Label2.Text = "Please enter a string from 1 to 8 characters";
+        }
+        else {             
+            perm(s);
+        }
+
+        txt1.Text = "";
+    }
+    protected void perm(string str)
+    {
+        perm("", str);
+        foreach(String s1 in duplicate)
+        {
+            ListBox1.Items.Add(s1);
+        }
+        Label2.Text = duplicate.Count + " no of anagrams.";
+    } 
+    protected void perm(string pre,string str)
+    {
+        
+        int len = str.Length;
+        if (len == 0)
+        {
+            if (CheckBox1.Checked)
+            {
+                if (!duplicate.Contains(pre))
+                {
+                    duplicate.Add(pre);
+                    
+                }
+            }
+            else
+            {
+                duplicate.Add(pre);
+                
+            }
+
+        }
+        else
+        {
+            for(int i=0;i< len; i++)
+            {
+                perm(pre + str[i], str.Substring(0, i) + str.Substring(i + 1));
+            }
+        }
+      
+    }   
+    
+
+
+
+
+    protected void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void CheckBox1_CheckedChanged(object sender, EventArgs e)
+    {
+
+    }
+}
