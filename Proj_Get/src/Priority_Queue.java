99ca92891b69dc437416a173fcdb9c207968dc71
hitesh3004 <hnlaware@bu.edu>
2016-12-15 01:41:14 -0500

    used comparator in collections

--- Proj_Get/src/Priority_Queue.java
+++ Proj_Get/src/Priority_Queue.java
@@ -1,16 +1,36 @@
-import java.util.PriorityQueue;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.Iterator;
+import java.util.PriorityQueue;
 public class Priority_Queue {
 	
 	public static void main(String args[]){
 		Comparator<String> comparator = new StringLengthComparator();
-	    PriorityQueue<String> queue = new PriorityQueue<String>(10, comparator);
+	    PriorityQueue<String> queue = new PriorityQueue<String>(1, comparator);
 	    queue.add("short");
 	    queue.add("very long indeed");
 	    queue.add("medium");
 	    queue.add("s");
 	    while (queue.size() != 0){
 	    	System.out.println(queue.remove());
+	    }
+	    
+	    Iterator i = queue.iterator();
+	    
+//	    while(i.hasNext()){
+//	    	System.out.println(i.next());
+//	    }
+	    
+	    ArrayList<String> arr = new ArrayList<String>();
+	    arr.add("short");
+	    arr.add("very long indeed");
+	    arr.add("medium");
+	    arr.add("s");
+	    Collections.sort(arr,new StringLengthComparator());
+	    
+	    for(String s: arr){
+	    	System.out.println(s);
 	    }
 	}
 }
--- Proj_Get/src/StringLengthComparator.java
+++ Proj_Get/src/StringLengthComparator.java
@@ -18,4 +18,26 @@ public class StringLengthComparator implements Comparator<String>{
         }
         return 0;
     }
+	
+//	public int compare(String x,String y){
+//		
+//		int countx,county;
+//		countx = 0;
+//		county = 0;
+//		for(int i = 0;i < x.length();i++){
+//			if(x.charAt(i) == 's')
+//				countx++;
+//		}
+//		for(int i = 0;i < y.length();i++){
+//			if(y.charAt(i) == 's')
+//				county++;
+//		}
+//		
+//		if(countx > county)
+//			return 1;
+//		else if(countx < county)
+//			return -1;
+//			
+//		return 0;
+//	}
 }
